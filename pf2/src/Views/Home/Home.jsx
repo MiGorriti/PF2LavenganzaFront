46a3a10ab8cc3b80cfbc0404b2ec4e46fc1af162
MiGorriti <miguegorriti@gmail.com>
2023-09-26 20:08:05 -0300

    cards

---
+++ pf2/src/Views/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cards from "../../Components/Cards/Cards";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+};
+
+export default Home;
