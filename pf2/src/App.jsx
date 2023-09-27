eed876be33c9125892c8d947430db1d8c5a0250c
MiGorriti <miguegorriti@gmail.com>
2023-09-27 11:59:29 -0300

    detail

--- pf2/src/App.jsx
+++ pf2/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Views/Home/Home";
 import Navbar from "./Components/Navbar/NavBar";
 import Footer from "./Components/Footer/Footer";
+import Detail from "./Views/Detail/Detail";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Navbar />
+        <Navbar />
         <Routes>
-        <Route path="/" exact component={Home} />
+          <Route exact path="/Home" element={<Home />}></Route>
+          <Route exact path="/Detail" element={<Detail />}></Route>
         </Routes>
         <Footer />
       </div>
