f6a9c35699a972928e72b981bb504cc477ed2e36
Giomy8 <giomarmesa@gmail.com>
2023-09-29 00:49:11 -0500

    LandingPage GM

--- pf2/src/main.jsx
+++ pf2/src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react'
+import React from 'react';
+import { createRoot } from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './Redux/store/store.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<Provider>
-<React.StrictMode>
-    <App />
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-
-</Provider>
-)
+);
