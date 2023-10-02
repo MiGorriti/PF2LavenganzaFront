b13b403bcac07ef5f7b1a72cdc4e42ca0af2c565
Omarx32 <rmarin.0812@gmail.com>
2023-10-02 14:03:46 -0430

    m

--- pf2/src/Views/FormRegister/Form.jsx
+++ pf2/src/Views/FormRegister/Form.jsx
@@ -58,7 +58,7 @@ const submitHandler = async (e) =>{
     <div className="container mx-auto">
       <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
         <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url(/imagenes/Fondolanding.png)' }}>
-          <h1 className="text-white text-3xl mb-3">Welcome</h1>
+          <h1 className="">Welcome</h1>
           <div>
             <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suspendisse aliquam varius rutrum purus maecenas ac <a href="#" className="text-purple-500 font-semibold">Learn more</a></p>
           </div>
