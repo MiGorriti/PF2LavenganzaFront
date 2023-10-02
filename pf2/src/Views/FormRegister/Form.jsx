194f2c9dd15fda03cbe54ed04e5e81102c831e24
Omarx32 <rmarin.0812@gmail.com>
2023-10-02 03:46:52 -0430

    test

--- pf2/src/Views/FormRegister/Form.jsx
+++ pf2/src/Views/FormRegister/Form.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export const FormUser = () => {
   return (
-    <div class='bg-blue-500 flex h-96'>
-      <div class='w-6/12 h-full flex justify-center items-center'  >
-        <img class='h-80 w-96' src="/imagenes/c1.jpg" alt="name" />
+    <div class="bg-blue-500 flex h-96">
+      <div class="w-6/12 h-full flex justify-center items-center">
+        <img class="h-80 w-96" src="/imagenes/c1.jpg" alt="name" />
       </div>
-      <div class='flex justify-center items-center'>
+      <div class="flex justify-center items-center">
         <section>
-        <p>Logo</p>
+          <p>Logo</p>
         </section>
         <section>
-          <input type="text" name="email" id="email" placeholder='Email'/>
-          <input type="text" name="password" id="password" placeholder='Password'/>
-          <button>
-            Login
-          </button>
-            <p>Forgot Password?</p>
-       </section>
+          <input type="text" name="email" id="email" placeholder="Email" />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <button>Login</button>
+          <p>Forgot Password?</p>
+        </section>
       </div>
     </div>
-  )
-}
+  );
+};
