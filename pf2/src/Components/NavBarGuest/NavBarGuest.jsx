f001fde9878a6bddd2edf5f5aecfa860fb3ff5dd
Giomy8 <giomarmesa@gmail.com>
2023-10-02 09:21:27 -0500

    arreglo de confictos en navbar GM

--- pf2/src/Components/NavBarGuest/NavBarGuest.jsx
+++ pf2/src/Components/NavBarGuest/NavBarGuest.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import React from 'react';
->>>>>>> 3d372cbac81204d4e450c53cbd6c20cba41fceb6
 import { Link } from 'react-router-dom';
 import styles from './NavBarGuest.module.css';
 //import { IconLogout, IconUserDown } from "@tabler/icons-react";
@@ -10,25 +7,6 @@ function NavBarGuest() {
   return (
     <nav>
       <div className={styles.navSup}>
-<<<<<<< HEAD
-      <div className={styles.menu}>
-      <img className={styles.logo} src="/imagenes/Wanderluxehomes.png" alt="Logo" />
-      <Link to="/about-us" className={styles.options}>
-        About Us
-      </Link>
-      <Link to="/recommendations" className={styles.options}>
-        Recommendations
-      </Link>
-      <Link to="/form" className={styles.options}>
-        Rent
-      </Link>
-      <Link to="/login" className={styles.options}>
-        Log In
-      </Link>
-      <Link to="/register" className={styles.options}>
-        Sign Up
-      </Link>
-=======
 
         <Link to="/">
           <img className={styles.logo} src="/imagenes/Wanderluxehomes.png" alt="Logo" />
@@ -51,7 +29,6 @@ function NavBarGuest() {
         <Link to="/register" className={styles.options}>
         Sign Up
         </Link>
->>>>>>> 3d372cbac81204d4e450c53cbd6c20cba41fceb6
       </div>
       </div>
     </nav>
