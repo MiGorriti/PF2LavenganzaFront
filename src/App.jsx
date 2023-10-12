ae355d4b13cde1be0fa7a294776a37c3d99ac581
AbareKiller100 <themr.camper213@gmail.com>
2023-10-12 18:03:08 -0300

    Formulario de reservas/Reservas realizadas

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { FormLogin } from "./Views/Login/Login";
 //import AdminDashboard from "./Views/DashBoard/AdminDashboard";
 import TerminosYCondiciones from "./Views/Terms/Terms";
 import Privacidad from "./Views/Terms/Privacy";
+import Reservations from "./Views/Reservations/Reservations";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -41,9 +42,10 @@ function App() {
           <Route exact path="/Home" element={<Home />}></Route>
           <Route path="/Form" element={<Form />}></Route>
           <Route exact path="/Register" element={<FormUser />}></Route>
-          <Route exact path="/Login" element={<FormLogin />}></Route>
+          <Route exact path="/Login" element={<FormLogin handleLogin={handleLogin}/>}></Route>
           <Route exact path="/Terms" element={<TerminosYCondiciones />}></Route>
           <Route exact path="/Privacy" element={<Privacidad />}></Route>
+          <Route exact path="/MyReservations" element={<Reservations/>}></Route>
         </Routes>
         <Footer />
       </div>
