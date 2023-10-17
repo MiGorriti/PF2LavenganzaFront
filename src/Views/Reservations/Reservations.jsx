c97a56299c2f2a02cb7f5775443f37b2725a127c
MiGorriti <miguegorriti@gmail.com>
2023-10-17 13:48:58 -0300

    mas arreglos

--- src/Views/Reservations/Reservations.jsx
+++ src/Views/Reservations/Reservations.jsx
@@ -1,3 +1,70 @@
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { cancel, getReservations } from "../../Redux/action/actions";
+// import { useState } from "react";
+// import CardsRes from "./CardsReserve/CardsRes";
+
+// const Reservations = () => {
+//   const [email, setEmail] = useState({});
+
+//   console.log(email);
+
+//   const meses = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+//   ];
+
+//   useEffect(() => {
+//     const storedUserData = localStorage.getItem("userData");
+//     if (storedUserData) {
+//       try {
+//         const userData = JSON.parse(storedUserData);
+//         setEmail(userData);
+//         // Utiliza la información del usuario aquí si es necesario
+//         dispatch(getReservations(userData.email));
+//       } catch (error) {
+//         console.error("Error al parsear JSON:", error);
+//       }
+//     }
+//   }, []);
+
+//   const dispatch = useDispatch();
+
+//   const reservations = useSelector((state) => state.reservations);
+
+//   return (
+//     <div>
+//       <h1>Your reservations</h1>
+//       <div>
+//         {!reservations.length ? (
+//           <h2>You don't have reservations</h2>
+//         ) : (
+//           <li>
+//             {reservations.map((res) => {
+//               return (
+//                 <ul>
+//                   <CardsRes />
+//                 </ul>
+//               );
+//             })}
+//           </li>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Reservations;
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { cancel, getReservations } from "../../Redux/action/actions";
@@ -43,6 +110,7 @@ const Reservations = () => {
 
   const cancelReserve = (event, id) => {
     dispatch(cancel(id));
+    window.location.reload(); // Recarga la página después de cancelar
   };
 
   return (
